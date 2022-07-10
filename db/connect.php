1b254d9746a99b827495d07bc2edad279d8b6767
Pham Van Tham <thampv@acb.com.vn>
2022-07-10 16:13:47 +0700

    second commit 10-07-2022 - 16:13

---
+++ db/connect.php
@@ -0,0 +1,19 @@
+<?php
+    // Tạo các biến chứa tham số kết nối database
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "salescrmregistration";
+
+    // Tạo kết nối thông qua câu lệnh new mysqli
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    /* Kiểm tra xem đã kết nối database thành công hay chưa
+    thông qua cú pháp đã được mặc định bởi mysql
+    */
+    if ($conn->connect_error) {
+        die ("Kết nối database không thành công: ".$conn->connect_error);
+    } else {
+        echo "Kết nối database thành công <br>";
+    }
+?>
--- registration.php
+++ registration.php
@@ -9,6 +9,18 @@
 <body>
     <h1>Đăng ký thành công</h1>
     <?php
+        // Tạo kết nối với database sử dụng câu lệnh require
+        require ('db/connect.php');
+
+        /* Kiểm tra xem nút "Đăng ký" (submit) đã được người dùng nhấn vào hay chưa
+        sử dụng giá trị của 'btn-reg' trong biến toàn cục $_POST['btn-reg']
+        của trình duyệt
+        ==> Lưu ý: biến toàn cục $_POST chính là một mảng mặc định của trình duyệt
+        */
+        if (isset($_POST['btn-reg'])) {
+            echo "Đã submit";
+            echo "<pre>";
+        }
         $registration_info = array();
         $registration_info[0] = $_POST['fullname'];
         $registration_info[1] = $_POST['userad'];
